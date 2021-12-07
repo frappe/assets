88f03086a80d2317b5946cebb047fdfeae403b90
GangaManoj <ganga.manoj98@gmail.com>
2021-12-08 05:16:03 +0530

    feat: Create Depreciation Schedule DocType

---
+++ assets/asset/doctype/depreciation_schedule_/depreciation_schedule_.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Ganga Manoj and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class DepreciationSchedule_(Document):
+	pass
