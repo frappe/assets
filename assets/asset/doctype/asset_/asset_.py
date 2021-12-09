876967bca63c6cb374851b3e14dd18b5913a8ed3
GangaManoj <ganga.manoj98@gmail.com>
2021-12-10 04:49:47 +0530

    fix: Validate purchase doc

--- assets/asset/doctype/asset_/asset_.py
+++ assets/asset/doctype/asset_/asset_.py
@@ -20,11 +20,23 @@ class Asset_(AccountsController):
 		self.validate_available_for_use_date()
 
 	def validate_purchase_document(self):
-		if (is_cwip_accounting_enabled(self.asset_category)
-			and not self.purchase_receipt
-			and self.purchase_invoice
-			and not frappe.db.get_value('Purchase Invoice', self.purchase_invoice, 'update_stock')):
-			frappe.throw(_("Update stock must be enable for the purchase invoice {0}").format(self.purchase_invoice))
+		if self.is_existing_asset:
+			if self.purchase_invoice:
+				frappe.throw(_("Purchase Invoice cannot be made against an existing asset {0}").format(self.name))
+
+		else:
+			purchase_doc = 'Purchase Invoice' if self.purchase_invoice else 'Purchase Receipt'
+			purchase_docname = self.purchase_invoice or self.purchase_receipt
+			purchase_doc = frappe.get_doc(purchase_doc, purchase_docname)
+
+			if purchase_doc.get('company') != self.company:
+				frappe.throw(_("Company of asset {0} and purchase document {1} doesn't match.").format(self.name, purchase_doc.get('name')))
+
+			if (is_cwip_accounting_enabled(self.asset_category)
+				and not self.purchase_receipt
+				and self.purchase_invoice
+				and not frappe.db.get_value('Purchase Invoice', self.purchase_invoice, 'update_stock')):
+				frappe.throw(_("Update stock must be enable for the purchase invoice {0}").format(self.purchase_invoice))
 
 	def validate_available_for_use_date(self):
 		if self.available_for_use_date and getdate(self.available_for_use_date) < getdate(self.purchase_date):
