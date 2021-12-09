528b646e56e4bfb021df1a6b4dc1766ddd942341
GangaManoj <ganga.manoj98@gmail.com>
2021-12-10 04:38:09 +0530

    fix: Validate asset values

--- assets/asset/doctype/asset_/asset_.py
+++ assets/asset/doctype/asset_/asset_.py
@@ -12,7 +12,23 @@ from erpnext.controllers.accounts_controller import AccountsController
 
 
 class Asset_(AccountsController):
-	pass
+	def validate(self):
+		self.validate_asset_values()
+
+	def validate_asset_values(self):
+		self.validate_purchase_document()
+		self.validate_available_for_use_date()
+
+	def validate_purchase_document(self):
+		if (is_cwip_accounting_enabled(self.asset_category)
+			and not self.purchase_receipt
+			and self.purchase_invoice
+			and not frappe.db.get_value('Purchase Invoice', self.purchase_invoice, 'update_stock')):
+			frappe.throw(_("Update stock must be enable for the purchase invoice {0}").format(self.purchase_invoice))
+
+	def validate_available_for_use_date(self):
+		if self.available_for_use_date and getdate(self.available_for_use_date) < getdate(self.purchase_date):
+			frappe.throw(_("Available-for-use Date should be after purchase date"))
 
 @frappe.whitelist()
 def get_finance_books(asset_category):
@@ -25,3 +41,6 @@ def get_finance_books(asset_category):
 		})
 
 	return books
+
+def is_cwip_accounting_enabled(asset_category):
+	return cint(frappe.db.get_value("Asset Category", asset_category, "enable_cwip_accounting"))
