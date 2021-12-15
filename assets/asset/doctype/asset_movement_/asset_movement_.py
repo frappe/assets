42fa64c10ccd18907d788c0260bf0023ec8f51fe
GangaManoj <ganga.manoj98@gmail.com>
2021-12-15 07:59:13 +0530

    fix: Validate asset

--- assets/asset/doctype/asset_movement_/asset_movement_.py
+++ assets/asset/doctype/asset_movement_/asset_movement_.py
@@ -6,4 +6,18 @@ from frappe import _
 from frappe.model.document import Document
 
 class AssetMovement_(Document):
-	pass
+	def validate(self):
+		self.validate_asset()
+
+	def validate_asset(self):
+		for asset in self.assets:
+			status, company = frappe.db.get_value("Asset", asset.asset, ["status", "company"])
+
+			if self.purpose == 'Transfer' and status in ("Draft", "Scrapped", "Sold"):
+				frappe.throw(_("Row {0}: {1} asset cannot be transferred.").format(asset.idx, status))
+
+			if company != self.company:
+				frappe.throw(_("{0} does not belong to company {1}.").format(asset.asset, self.company))
+
+			if not (asset.source_location or asset.target_location or asset.from_employee or asset.to_employee):
+				frappe.throw(_("Either location or employee must be entered."))
