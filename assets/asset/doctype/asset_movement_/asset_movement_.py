c7b5af37d53a3641c9b526e6302cfca85a350505
GangaManoj <ganga.manoj98@gmail.com>
2021-12-15 08:00:05 +0530

    fix: Validate asset movement

--- assets/asset/doctype/asset_movement_/asset_movement_.py
+++ assets/asset/doctype/asset_movement_/asset_movement_.py
@@ -8,6 +8,7 @@ from frappe.model.document import Document
 class AssetMovement_(Document):
 	def validate(self):
 		self.validate_asset()
+		self.validate_movement()
 
 	def validate_asset(self):
 		for asset in self.assets:
@@ -21,3 +22,66 @@ class AssetMovement_(Document):
 
 			if not (asset.source_location or asset.target_location or asset.from_employee or asset.to_employee):
 				frappe.throw(_("Either location or employee must be entered."))
+
+	def validate_movement(self):
+		for asset in self.assets:
+			if self.purpose in ['Transfer', 'Issue']:
+				self.validate_source_location(asset)
+
+				if self.purpose == 'Issue':
+					self.validate_asset_issue(asset)
+				if self.purpose == 'Transfer':
+					self.validate_asset_transfer(asset)
+
+			else:
+				self.validate_asset_receipt(asset)
+
+	def validate_source_location(self, asset):
+		current_location = frappe.db.get_value("Asset", asset.asset, "location")
+
+		if not asset.source_location:
+			if not current_location:
+				frappe.throw(_("Source Location is required for the Asset {0}").format(asset.asset))
+			else:
+				asset.source_location = current_location
+		else:
+			if current_location != asset.source_location:
+				frappe.throw(_("Asset {0} is currently located at {1}, not {2}.").
+					format(asset.asset, current_location, asset.source_location))
+
+	def validate_asset_issue(self, asset):
+		if asset.target_location:
+			frappe.throw(_("Issuing cannot be done to a location. \
+				Please enter employee who has issued Asset {0}").
+				format(asset.asset), title="Incorrect Movement Purpose")
+
+		if not asset.to_employee:
+			frappe.throw(_("Employee is required while issuing Asset {0}").format(asset.asset))
+
+	def validate_asset_transfer(self, asset):
+		if asset.to_employee:
+			frappe.throw(_("Transferring cannot be done to an Employee. \
+				Please enter location where Asset {0} has to be transferred").
+				format(asset.asset), title="Incorrect Movement Purpose")
+
+		if not asset.target_location:
+			frappe.throw(_("Target Location is required while transferring Asset {0}").format(asset.asset))
+
+		if asset.source_location == asset.target_location:
+			frappe.throw(_("Source and Target Location cannot be same"))
+
+	def validate_asset_receipt(self, asset):
+		# only when asset is bought and first entry is made
+		if not asset.source_location and not (asset.target_location or asset.to_employee):
+			frappe.throw(_("Target Location or To Employee is required while receiving Asset {0}").format(asset.asset))
+
+		elif asset.source_location:
+			if asset.target_location and not asset.from_employee:
+				frappe.throw(_("From employee is required while receiving Asset {0} at a target location").format(asset.asset))
+
+			if asset.from_employee and not asset.target_location:
+				frappe.throw(_("Target Location is required while receiving Asset {0} from an employee").format(asset.asset))
+
+			if asset.to_employee and asset.target_location:
+				frappe.throw(_("Asset {0} cannot be received at a location and \
+					given to employee in a single movement").format(asset.asset))
