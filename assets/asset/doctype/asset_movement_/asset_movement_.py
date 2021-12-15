ff435bc1f628d9e0fc1ae2e69702c47ccd2d76f6
GangaManoj <ganga.manoj98@gmail.com>
2021-12-15 07:58:19 +0530

    feat: Create Asset Movement DocType

---
+++ assets/asset/doctype/asset_movement_/asset_movement_.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2021, Ganga Manoj and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.model.document import Document
+
+class AssetMovement_(Document):
+	pass
---
+++ assets/asset/doctype/asset_movement_/test_asset_movement_.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Ganga Manoj and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestAssetMovement_(unittest.TestCase):
+	pass
