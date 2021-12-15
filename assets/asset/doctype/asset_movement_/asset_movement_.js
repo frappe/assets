e06ee8a08a15b831dc72b45d2eb9695ddf7973f2
GangaManoj <ganga.manoj98@gmail.com>
2021-12-15 08:13:50 +0530

    feat: Filter values by Company

--- assets/asset/doctype/asset_movement_/asset_movement_.js
+++ assets/asset/doctype/asset_movement_/asset_movement_.js
@@ -2,7 +2,57 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Asset Movement_', {
-	// refresh: function(frm) {
+	setup: (frm) => {
+		frm.set_query("to_employee", "assets", (doc) => {
+			return {
+				filters: {
+					company: doc.company
+				}
+			};
+		})
+		frm.set_query("from_employee", "assets", (doc) => {
+			return {
+				filters: {
+					company: doc.company
+				}
+			};
+		})
+		frm.set_query("reference_name", (doc) => {
+			return {
+				filters: {
+					company: doc.company,
+					docstatus: 1
+				}
+			};
+		})
+		frm.set_query("reference_doctype", () => {
+			return {
+				filters: {
+					name: ["in", ["Purchase Receipt", "Purchase Invoice"]]
+				}
+			};
+		}),
+		frm.set_query("asset", "assets", () => {
+			return {
+				filters: {
+					status: ["not in", ["Draft"]]
+				}
+			}
+		})
+	},
+});
 
-	// }
+frappe.ui.form.on('Asset Movement Item', {
+	asset: function(frm, cdt, cdn) {
+		// on manual entry of an asset auto sets their source location / employee
+		const asset_name = locals[cdt][cdn].asset;
+		if (asset_name){
+			frappe.db.get_doc('Asset', asset_name).then((asset_doc) => {
+				if(asset_doc.location) frappe.model.set_value(cdt, cdn, 'source_location', asset_doc.location);
+				if(asset_doc.custodian) frappe.model.set_value(cdt, cdn, 'from_employee', asset_doc.custodian);
+			}).catch((err) => {
+				console.log(err); // eslint-disable-line
+			});
+		}
+	}
 });
