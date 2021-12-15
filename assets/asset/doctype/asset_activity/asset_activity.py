b15a1e8d8ce59b2325d51957c3e8d56427225118
GangaManoj <ganga.manoj98@gmail.com>
2021-12-16 04:22:51 +0530

    fix: Create Asset Activity

--- assets/asset/doctype/asset_activity/asset_activity.py
+++ assets/asset/doctype/asset_activity/asset_activity.py
@@ -1,8 +1,26 @@
 # Copyright (c) 2021, Ganga Manoj and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class AssetActivity(Document):
-	pass
+	def validate(self):
+		self.validate_activity_date()
+
+	def validate_activity_date(self):
+		purchase_date = frappe.db.get_value("Asset", self.asset, 'purchase_date')
+
+		if self.activity_date < purchase_date:
+			frappe.throw(_("Asset Activity cannot be performed before {0}").format(purchase_date))
+
+def create_asset_activity(asset, activity_date, activity_type, notes=None):
+	asset_activity = frappe.get_doc({
+		'doctype': 'Asset Activity',
+		'asset': asset,
+		'activity_date': activity_date,
+		'activity_type': activity_type,
+		'notes': notes
+	})
+	asset_activity.submit()
