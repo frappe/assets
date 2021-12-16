59e707feab82f8abb3747f13039169c5728ea0df
GangaManoj <ganga.manoj98@gmail.com>
2021-12-16 08:36:17 +0530

    fix: Add Asset Serial No field

--- assets/asset/doctype/asset_activity/asset_activity.py
+++ assets/asset/doctype/asset_activity/asset_activity.py
@@ -3,6 +3,7 @@
 
 import frappe
 from frappe import _
+from frappe.utils import getdate
 from frappe.model.document import Document
 
 class AssetActivity(Document):
@@ -10,15 +11,16 @@ class AssetActivity(Document):
 		self.validate_activity_date()
 
 	def validate_activity_date(self):
-		purchase_date = frappe.db.get_value("Asset", self.asset, 'purchase_date')
+		purchase_date = frappe.db.get_value("Asset_", self.asset, 'purchase_date')
 
-		if self.activity_date < purchase_date:
+		if getdate(self.activity_date) < purchase_date:
 			frappe.throw(_("Asset Activity cannot be performed before {0}").format(purchase_date))
 
-def create_asset_activity(asset, activity_date, activity_type, reference_doctype, reference_docname, notes=None):
+def create_asset_activity(asset, activity_date, activity_type, reference_doctype, reference_docname, asset_serial_no=None, notes=None):
 	asset_activity = frappe.get_doc({
 		'doctype': 'Asset Activity',
 		'asset': asset,
+		'asset_serial_no': asset_serial_no,
 		'activity_date': activity_date,
 		'activity_type': activity_type,
 		'reference_doctype': reference_doctype,
