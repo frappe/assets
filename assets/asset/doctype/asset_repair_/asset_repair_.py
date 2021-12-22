c93524f186ddaf5659cc5650bde9b8bf870a745e
GangaManoj <ganga.manoj98@gmail.com>
2021-12-22 10:24:11 +0530

    feat: Change Asset status to Out of Order on creating Asset Repair

--- assets/asset/doctype/asset_repair_/asset_repair_.py
+++ assets/asset/doctype/asset_repair_/asset_repair_.py
@@ -1,8 +1,16 @@
 # Copyright (c) 2021, Ganga Manoj and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class AssetRepair_(Document):
-	pass
+	def validate(self):
+		self.asset_doc = frappe.get_doc('Asset_', self.asset)
+		self.update_status()
+
+	def update_status(self):
+		if self.repair_status == 'Pending':
+			frappe.db.set_value('Asset_', self.asset, 'status', 'Out of Order')
+		else:
+			self.asset_doc.set_status()
