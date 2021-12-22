3ba0bece260b76c25e6a05e31148dfe839d8a7a7
GangaManoj <ganga.manoj98@gmail.com>
2021-12-22 10:36:27 +0530

    feat: Set total value for each item consumed during Asset Repair

--- assets/asset/doctype/asset_repair_/asset_repair_.py
+++ assets/asset/doctype/asset_repair_/asset_repair_.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe.utils import flt
 from frappe.model.document import Document
 
 class AssetRepair_(Document):
@@ -9,8 +10,15 @@ class AssetRepair_(Document):
 		self.asset_doc = frappe.get_doc('Asset_', self.asset)
 		self.update_status()
 
+		if self.get('items'):
+			self.set_total_value()
+
 	def update_status(self):
 		if self.repair_status == 'Pending':
 			frappe.db.set_value('Asset_', self.asset, 'status', 'Out of Order')
 		else:
 			self.asset_doc.set_status()
+
+	def set_total_value(self):
+		for item in self.get('items'):
+			item.amount = flt(item.rate) * flt(item.qty)
