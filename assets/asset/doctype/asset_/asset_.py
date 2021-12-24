581d8518b667fe84113dda5f58e859c224df3e25
GangaManoj <ganga.manoj98@gmail.com>
2021-12-24 11:56:29 +0530

    fix: Get asset account

--- assets/asset/doctype/asset_/asset_.py
+++ assets/asset/doctype/asset_/asset_.py
@@ -11,6 +11,7 @@ from frappe.utils import (
 	nowdate,
 )
 from erpnext.controllers.accounts_controller import AccountsController
+from assets.asset.doctype.asset_category_.asset_category_ import get_asset_category_account
 from assets.asset.doctype.asset_activity.asset_activity import create_asset_activity
 
 
@@ -303,3 +304,24 @@ def get_default_finance_book(company=None):
 			company,  "default_finance_book")
 
 	return frappe.local.default_finance_book[company]
+
+def get_asset_account(account_name, asset=None, asset_category=None, company=None):
+	account = None
+	if asset:
+		account = get_asset_category_account(account_name, asset=asset,
+				asset_category = asset_category, company = company)
+
+	if not asset and not account:
+		account = get_asset_category_account(account_name, asset_category = asset_category, company = company)
+
+	if not account:
+		account = frappe.get_cached_value('Company',  company,  account_name)
+
+	if not account:
+		if not asset_category:
+			frappe.throw(_("Set {0} in company {1}").format(account_name.replace('_', ' ').title(), company))
+		else:
+			frappe.throw(_("Set {0} in asset category {1} or company {2}")
+				.format(account_name.replace('_', ' ').title(), asset_category, company))
+
+	return account
