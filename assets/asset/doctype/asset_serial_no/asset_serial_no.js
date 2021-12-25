98c14cdcf0ef51f84d98c16b0d915582faa6825a
GangaManoj <ganga.manoj98@gmail.com>
2021-12-25 11:57:41 +0530

    feat: Display available-for-use date and finance_books and make them mandatory only if the Asset is depreciable

--- assets/asset/doctype/asset_serial_no/asset_serial_no.js
+++ assets/asset/doctype/asset_serial_no/asset_serial_no.js
@@ -2,7 +2,30 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Asset Serial No', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		frm.trigger('toggle_depreciation_fields');
+	},
 
-	// }
+	asset: (frm) => {
+		frm.trigger('toggle_depreciation_fields');
+	},
+
+	toggle_depreciation_fields: (frm) => {
+		if (frm.doc.asset) {
+			frappe.db.get_doc('Asset_', frm.doc.asset).then(asset_doc => {
+				if (asset_doc.calculate_depreciation) {
+					frm.set_df_property('available_for_use_date', 'read_only', 0);
+					frm.set_df_property('finance_books', 'read_only', 0);
+					frm.toggle_reqd('available_for_use_date', 1);
+					frm.toggle_reqd('finance_books', 1);
+				}
+				else {
+					frm.set_df_property('available_for_use_date', 'read_only', 1);
+					frm.set_df_property('finance_books', 'read_only', 1);
+					frm.toggle_reqd('available_for_use_date', 0);
+					frm.toggle_reqd('finance_books', 0);
+				}
+			});
+		}
+	},
 });
