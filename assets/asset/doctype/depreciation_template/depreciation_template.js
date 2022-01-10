14c486f3730a99854c9896589172b18de4307911
GangaManoj <ganga.manoj98@gmail.com>
2022-01-10 18:40:40 +0530

    fix: Rename 'Finance Book_' as 'Depreciation Template'

--- assets/asset/doctype/depreciation_template/depreciation_template.js
+++ assets/asset/doctype/depreciation_template/depreciation_template.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, Ganga Manoj and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Finance Book_', {
+frappe.ui.form.on('Depreciation Template', {
 	// refresh: function(frm) {
 
 	// }
--- assets/asset/doctype/depreciation_template/depreciation_template.py
+++ assets/asset/doctype/depreciation_template/depreciation_template.py
@@ -4,5 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-class FinanceBook_(Document):
+class DepreciationTemplate(Document):
 	pass
