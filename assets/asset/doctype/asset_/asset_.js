12674c06e3a830101f61689691f70466ed981aa0
GangaManoj <ganga.manoj98@gmail.com>
2022-01-12 21:16:45 +0530

    feat: Add buttons

--- assets/asset/doctype/asset_/asset_.js
+++ assets/asset/doctype/asset_/asset_.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2021, Ganga Manoj and contributors
 // For license information, please see license.txt
 
+frappe.provide("erpnext.accounts.dimensions");
+
 frappe.ui.form.on('Asset_', {
 	onload: function(frm) {
 		frm.set_query("item_code", function() {
@@ -49,6 +51,67 @@ frappe.ui.form.on('Asset_', {
 
 		if (frm.doc.docstatus == 0) {
 			frm.toggle_reqd("finance_books", frm.doc.calculate_depreciation);
+		}
+
+		if (frm.doc.docstatus == 1) {
+			if (frm.doc.is_serialized_asset) {
+				frm.add_custom_button(__("Create Serial Numbers"), function() {
+					frm.trigger("create_asset_serial_nos");
+				});
+			} else {
+				if (in_list(["Submitted", "Partially Depreciated", "Fully Depreciated"], frm.doc.status)) {
+					frm.add_custom_button(__("Transfer Asset"), function() {
+						erpnext.asset.transfer_asset(frm);
+					}, __("Manage"));
+
+					frm.add_custom_button(__("Scrap Asset"), function() {
+						erpnext.asset.scrap_asset(frm);
+					}, __("Manage"));
+
+					frm.add_custom_button(__("Sell Asset"), function() {
+						frm.trigger("make_sales_invoice");
+					}, __("Manage"));
+
+				} else if (frm.doc.status=='Scrapped') {
+					frm.add_custom_button(__("Restore Asset"), function() {
+						erpnext.asset.restore_asset(frm);
+					}, __("Manage"));
+				}
+
+				if (frm.doc.maintenance_required && !frm.doc.maintenance_schedule) {
+					frm.add_custom_button(__("Maintain Asset"), function() {
+						frm.trigger("create_asset_maintenance");
+					}, __("Manage"));
+				}
+
+				frm.add_custom_button(__("Repair Asset"), function() {
+					frm.trigger("create_asset_repair");
+				}, __("Manage"));
+
+				if (frm.doc.status != 'Fully Depreciated') {
+					frm.add_custom_button(__("Adjust Asset Value"), function() {
+						frm.trigger("create_asset_value_adjustment");
+					}, __("Manage"));
+				}
+
+				if (!frm.doc.calculate_depreciation) {
+					frm.add_custom_button(__("Create Depreciation Entry"), function() {
+						frm.trigger("make_journal_entry");
+					}, __("Manage"));
+				}
+
+				if (frm.doc.purchase_receipt || !frm.doc.is_existing_asset) {
+					frm.add_custom_button(__("View General Ledger"), function() {
+						frappe.route_options = {
+							"voucher_no": frm.doc.name,
+							"from_date": frm.doc.available_for_use_date,
+							"to_date": frm.doc.available_for_use_date,
+							"company": frm.doc.company
+						};
+						frappe.set_route("query-report", "General Ledger");
+					}, __("Manage"));
+				}
+			}
 		}
 	},
 
