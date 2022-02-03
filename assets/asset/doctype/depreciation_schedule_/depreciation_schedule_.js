203e142ca5f371725772bf4e54151b45b9f4947e
GangaManoj <ganga.manoj98@gmail.com>
2022-02-04 00:14:24 +0530

    fix: Filter Serial No list by Asset

--- assets/asset/doctype/depreciation_schedule_/depreciation_schedule_.js
+++ assets/asset/doctype/depreciation_schedule_/depreciation_schedule_.js
@@ -2,6 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Depreciation Schedule_', {
+	setup: function(frm) {
+		frm.fields_dict.serial_no.get_query = function(doc) {
+			return {
+				filters: {
+					'asset': doc.asset
+				}
+			};
+		};
+	},
+
 	refresh: function(frm) {
 		if (frm.doc.status == "Active") {
 			frm.add_custom_button(__("Post Depreciation Entries"), function() {
