bef0fc7582fb34944010b9eb8ff6161538d5d3f3
GangaManoj <ganga.manoj98@gmail.com>
2022-02-04 00:11:11 +0530

    feat: Add 'Post Depreciation Entries' button

--- assets/asset/doctype/depreciation_schedule_/depreciation_schedule_.js
+++ assets/asset/doctype/depreciation_schedule_/depreciation_schedule_.js
@@ -2,7 +2,15 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Depreciation Schedule_', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		if (frm.doc.status == "Active") {
+			frm.add_custom_button(__("Post Depreciation Entries"), function() {
+				frm.trigger("post_depreciation_entries");
+			});
+		}
+	},
 
-	// }
+	post_depreciation_entries: function(frm) {
+
+	}
 });
