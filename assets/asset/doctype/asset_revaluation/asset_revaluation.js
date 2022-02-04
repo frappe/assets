2ae62a96eef63dacdb830ffd2842166a55285cf1
GangaManoj <ganga.manoj98@gmail.com>
2022-02-04 21:56:17 +0530

    feat: Create Asset Revaluation

---
+++ assets/asset/doctype/asset_revaluation/asset_revaluation.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Ganga Manoj and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Asset Revaluation', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ assets/asset/doctype/asset_revaluation/test_asset_revaluation.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Ganga Manoj and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestAssetRevaluation(unittest.TestCase):
+	pass
