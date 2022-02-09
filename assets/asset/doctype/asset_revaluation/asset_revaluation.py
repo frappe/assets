f598500641e3c83e6359cba1e9cf4f029b5b97c7
GangaManoj <ganga.manoj98@gmail.com>
2022-02-10 03:52:07 +0530

    fix: Validate transaction date, serial no and num of assets

--- assets/asset/doctype/asset_revaluation/asset_revaluation.py
+++ assets/asset/doctype/asset_revaluation/asset_revaluation.py
@@ -2,10 +2,37 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import getdate, formatdate
+
+from assets.asset.doctype.asset_repair_.asset_repair_ import (
+	validate_num_of_assets,
+	validate_serial_no
+)
 
 class AssetRevaluation(Document):
-	pass
+	def validate(self):
+		self.validate_asset_values()
+
+	def validate_asset_values(self):
+		purchase_date, is_serialized_asset, num_of_assets = frappe.db.get_value(
+			"Asset_",
+			self.asset,
+			["purchase_date", "is_serialized_asset", "num_of_assets"]
+		)
+
+		self.validate_transaction_date_against_purchase_date(purchase_date)
+
+		if is_serialized_asset:
+			validate_serial_no(self)
+		else:
+			validate_num_of_assets(self, num_of_assets)
+
+	def validate_transaction_date_against_purchase_date(self, purchase_date):
+		if getdate(self.date) < getdate(purchase_date):
+			frappe.throw(_("Asset Revaluation cannot be posted before Asset's purchase date: <b>{0}</b>.")
+				.format(formatdate(purchase_date)), title = "Invalid Date")
 
 @frappe.whitelist()
 def get_current_asset_value(asset, serial_no=None, finance_book=None):
