c775ccf645b097f25306eb48ca2935ebd217dd15
GangaManoj <ganga.manoj98@gmail.com>
2022-02-16 19:51:57 +0530

    fix: Hide serial_no and make it non-mandatory if the asset is non-serialized

--- assets/asset/doctype/depreciation_entry/depreciation_entry.js
+++ assets/asset/doctype/depreciation_entry/depreciation_entry.js
@@ -2,7 +2,28 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Depreciation Entry', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		frm.trigger("toggle_display_and_reqd_for_serial_no");
+	},
 
-	// }
+	asset: (frm) => {
+		frm.trigger("toggle_display_and_reqd_for_serial_no");
+	},
+
+	toggle_display_and_reqd_for_serial_no: (frm) => {
+		if (frm.doc.asset) {
+			frappe.db.get_value('Asset_', frm.doc.asset, ['is_serialized_asset'], (r) => {
+				if (r && r.is_serialized_asset) {
+					frm.set_df_property('serial_no', 'hidden', 0);
+					frm.set_df_property('serial_no', 'reqd', 1);
+				} else {
+					frm.set_df_property('serial_no', 'hidden', 1);
+					frm.set_df_property('serial_no', 'reqd', 0);
+					frm.set_value('serial_no', '');
+				}
+			});
+		} else {
+			frm.set_df_property('serial_no', 'hidden', 1);
+		}
+	},
 });
