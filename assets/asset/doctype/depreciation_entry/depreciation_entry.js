1a90d41df4dfb0495a2f5f652eef817f44c7cb76
GangaManoj <ganga.manoj98@gmail.com>
2022-02-16 20:22:43 +0530

    fix: Toggle display for finance bookfield

--- assets/asset/doctype/depreciation_entry/depreciation_entry.js
+++ assets/asset/doctype/depreciation_entry/depreciation_entry.js
@@ -31,10 +31,12 @@ frappe.ui.form.on('Depreciation Entry', {
 
 	refresh: function(frm) {
 		frm.trigger("toggle_display_and_reqd_for_serial_no");
+		frm.trigger("toggle_display_for_finance_book");
 	},
 
 	asset: (frm) => {
 		frm.trigger("toggle_display_and_reqd_for_serial_no");
+		frm.trigger("toggle_display_for_finance_book");
 	},
 
 	toggle_display_and_reqd_for_serial_no: (frm) => {
@@ -53,4 +55,34 @@ frappe.ui.form.on('Depreciation Entry', {
 			frm.set_df_property('serial_no', 'hidden', 1);
 		}
 	},
+
+	toggle_display_for_finance_book: (frm) => {
+		if (frm.doc.asset) {
+			frappe.db.get_value('Asset_', frm.doc.asset, ['calculate_depreciation'], (r) => {
+				if (r && r.calculate_depreciation) {
+					if (frm.doc.serial_no) {
+						var doctype = 'Asset Serial No';
+						var docname = frm.doc.serial_no;
+					} else {
+						var doctype = 'Asset_';
+						var docname = frm.doc.asset;
+					}
+
+					frappe.db.get_doc(doctype, docname).then(data => {
+						if (data.finance_books.length > 1) {
+							frm.set_df_property('finance_book', 'hidden', 0);
+							frm.set_df_property('finance_book', 'reqd', 1);
+						} else {
+							frm.set_df_property('finance_book', 'hidden', 1);
+							frm.set_df_property('finance_book', 'reqd', 0);
+						}
+					})
+				} else {
+					frm.set_df_property('finance_book', 'hidden', 1);
+				}
+			})
+		} else {
+			frm.set_df_property('finance_book', 'hidden', 1);
+		}
+	}
 });
