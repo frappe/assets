b9b94ec6a751f13f3211ba6ed4d4bb95cd677bbb
GangaManoj <ganga.manoj98@gmail.com>
2022-02-16 19:31:46 +0530

    feat: Create Depreciation Entry doctype

---
+++ assets/asset/doctype/depreciation_entry/depreciation_entry.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Ganga Manoj and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Depreciation Entry', {
+	// refresh: function(frm) {
+
+	// }
+});
