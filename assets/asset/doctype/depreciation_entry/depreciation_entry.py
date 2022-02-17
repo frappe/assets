d4bb8c7043e7b3ec3e67b16fb1ffead588ca01d1
GangaManoj <ganga.manoj98@gmail.com>
2022-02-17 18:41:27 +0530

    fix: Validate Depreciation Amount

--- assets/asset/doctype/depreciation_entry/depreciation_entry.py
+++ assets/asset/doctype/depreciation_entry/depreciation_entry.py
@@ -1,8 +1,16 @@
 # Copyright (c) 2022, Ganga Manoj and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.model.document import Document
+import frappe
+from frappe import _
 
-class DepreciationEntry(Document):
-	pass
+from erpnext.controllers.accounts_controller import AccountsController
+
+class DepreciationEntry(AccountsController):
+	def validate(self):
+		self.validate_depreciation_amount()
+
+	def validate_depreciation_amount(self):
+		if self.depreciation_amount <= 0:
+			frappe.throw(_("Depreciation Amount must be greater than zero."), title = _("Invalid Amount"))
+
