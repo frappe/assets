a275f0160ed0f0c98112a400c152eb499e5981da
GangaManoj <ganga.manoj98@gmail.com>
2022-02-17 19:08:53 +0530

    fix: Validate Reference Doc

--- assets/asset/doctype/depreciation_entry/depreciation_entry.py
+++ assets/asset/doctype/depreciation_entry/depreciation_entry.py
@@ -9,8 +9,13 @@ from erpnext.controllers.accounts_controller import AccountsController
 class DepreciationEntry(AccountsController):
 	def validate(self):
 		self.validate_depreciation_amount()
+		self.validate_reference_doc()
 
 	def validate_depreciation_amount(self):
 		if self.depreciation_amount <= 0:
 			frappe.throw(_("Depreciation Amount must be greater than zero."), title = _("Invalid Amount"))
 
+	def validate_reference_doc(self):
+		if self.reference_doctype not in ["Asset_", "Asset Serial No", "Depreciation Schedule_"]:
+			frappe.throw(_("Reference Document can only be an Asset, Asset Serial No or Depreciation Schedule."),
+				title = _("Invalid Reference"))
