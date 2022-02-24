5bda3e4af5b4f4640b87a322c7151435c4ddd511
GangaManoj <ganga.manoj98@gmail.com>
2022-02-24 17:04:49 +0530

    fix: Change visibility of Depreciation Details based on Asset

--- assets/asset/doctype/asset_serial_no/asset_serial_no.js
+++ assets/asset/doctype/asset_serial_no/asset_serial_no.js
@@ -22,24 +22,47 @@ frappe.ui.form.on('Asset Serial No', {
 
 	toggle_depreciation_fields: (frm) => {
 		if (frm.doc.asset) {
-			frappe.db.get_doc('Asset_', frm.doc.asset).then(asset_doc => {
-				if (asset_doc.calculate_depreciation) {
-					frm.set_df_property('available_for_use_date', 'read_only', 0);
-					frm.set_df_property('finance_books', 'read_only', 0);
+			frappe.db.get_value('Asset_', frm.doc.asset, ['calculate_depreciation', 'is_existing_asset'], (r) => {
+				if (r && r.calculate_depreciation) {
+					frm.set_df_property('available_for_use_date', 'hidden', 0);
+					frm.set_df_property('depreciation_posting_start_date', 'hidden', 0);
+					frm.set_df_property('salvage_value', 'hidden', 0);
+					frm.set_df_property('finance_books', 'hidden', 0);
+
 					frm.toggle_reqd('available_for_use_date', 1);
+					frm.toggle_reqd('depreciation_posting_start_date', 1);
+					frm.toggle_reqd('salvage_value', 1);
 					frm.toggle_reqd('finance_books', 1);
+
+					if (r.is_existing_asset) {
+						frm.set_df_property('opening_accumulated_depreciation', 'hidden', 0);
+					} else {
+						frm.set_df_property('opening_accumulated_depreciation', 'hidden', 1);
+					}
 				}
 				else {
-					frm.set_df_property('available_for_use_date', 'read_only', 1);
-					frm.set_df_property('finance_books', 'read_only', 1);
+					frm.set_df_property('available_for_use_date', 'hidden', 1);
+					frm.set_df_property('depreciation_posting_start_date', 'hidden', 1);
+					frm.set_df_property('opening_accumulated_depreciation', 'hidden', 1);
+					frm.set_df_property('salvage_value', 'hidden', 1);
+					frm.set_df_property('finance_books', 'hidden', 1);
+
 					frm.toggle_reqd('available_for_use_date', 0);
+					frm.toggle_reqd('depreciation_posting_start_date', 0);
+					frm.toggle_reqd('salvage_value', 0);
 					frm.toggle_reqd('finance_books', 0);
 				}
 			});
 		} else {
-			frm.set_df_property('available_for_use_date', 'read_only', 1);
-			frm.set_df_property('finance_books', 'read_only', 1);
+			frm.set_df_property('available_for_use_date', 'hidden', 1);
+			frm.set_df_property('depreciation_posting_start_date', 'hidden', 1);
+			frm.set_df_property('opening_accumulated_depreciation', 'hidden', 1);
+			frm.set_df_property('salvage_value', 'hidden', 1);
+			frm.set_df_property('finance_books', 'hidden', 1);
+
 			frm.toggle_reqd('available_for_use_date', 0);
+			frm.toggle_reqd('depreciation_posting_start_date', 0);
+			frm.toggle_reqd('salvage_value', 0);
 			frm.toggle_reqd('finance_books', 0);
 		}
 	},
