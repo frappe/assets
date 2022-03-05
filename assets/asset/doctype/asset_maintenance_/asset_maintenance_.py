0c61575d8ecca52315590d195325a3841147b03f
GangaManoj <ganga.manoj98@gmail.com>
2022-03-06 04:33:08 +0530

    feat: Assign tasks

--- assets/asset/doctype/asset_maintenance_/asset_maintenance_.py
+++ assets/asset/doctype/asset_maintenance_/asset_maintenance_.py
@@ -3,12 +3,16 @@
 
 import frappe
 from frappe import _
+from frappe.desk.form import assign_to
 from frappe.model.document import Document
 from frappe.utils import getdate
 
 class AssetMaintenance_(Document):
 	def validate(self):
 		self.validate_tasks()
+
+	def on_update(self):
+		self.assign_tasks()
 
 	def validate_tasks(self):
 		for task in self.get("asset_maintenance_tasks"):
@@ -30,3 +34,36 @@ class AssetMaintenance_(Document):
 			frappe.throw(_("Row #{0}: Please asign task {1} to a team member.")
 				.format(task.idx, task.maintenance_task))
 
+	def assign_tasks(self):
+		for task in self.get("asset_maintenance_tasks"):
+			self.assign_task(task)
+
+	def assign_task(self, task):
+		team_member = frappe.db.get_value("User", task.assign_to, "email")
+
+		args = {
+			"doctype" : "Asset Maintenance_",
+			"assign_to" : [team_member],
+			"name" : self.name,
+			"description" : task.maintenance_task,
+			"date" : task.next_due_date
+		}
+
+		if not self.have_todos_already_been_created(args):
+			assign_to.add(args)
+
+	def have_todos_already_been_created(self, args):
+		todos = frappe.get_all(
+			"ToDo",
+			filters = {
+				"reference_type": args["doctype"],
+				"reference_name": args["name"],
+				"status": "Open",
+				"owner": args["assign_to"]
+			}
+		)
+
+		if todos:
+			return True
+		else:
+			return False
