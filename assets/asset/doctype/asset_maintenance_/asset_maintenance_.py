bc7c6bef9309ec8f989e6b57ea4f17f68dced734
GangaManoj <ganga.manoj98@gmail.com>
2022-03-06 05:03:44 +0530

    feat: Sync maintenance tasks

--- assets/asset/doctype/asset_maintenance_/asset_maintenance_.py
+++ assets/asset/doctype/asset_maintenance_/asset_maintenance_.py
@@ -13,6 +13,7 @@ class AssetMaintenance_(Document):
 
 	def on_update(self):
 		self.assign_tasks()
+		self.sync_maintenance_tasks()
 
 	def validate_tasks(self):
 		for task in self.get("asset_maintenance_tasks"):
@@ -67,3 +68,69 @@ class AssetMaintenance_(Document):
 			return True
 		else:
 			return False
+
+	def sync_maintenance_tasks(self):
+		tasks_names = []
+		for task in self.get("asset_maintenance_tasks"):
+			tasks_names.append(task.name)
+			self.update_maintenance_log(task)
+
+		self.cancel_maintenance_logs_for_removed_tasks(tasks_names)
+
+	def update_maintenance_log(self, task):
+		asset_maintenance_log = self.get_maintenance_log(task)
+
+		if not asset_maintenance_log:
+			self.create_new_maintenance_log(task)
+		else:
+			maintenance_log = frappe.get_doc("Asset Maintenance Log_", asset_maintenance_log)
+			maintenance_log.assign_to_name = task.assign_to_name
+			maintenance_log.has_certificate = task.certificate_required
+			maintenance_log.description = task.description
+			maintenance_log.periodicity = str(task.periodicity)
+			maintenance_log.maintenance_type = task.maintenance_type
+			maintenance_log.due_date = task.next_due_date
+			maintenance_log.save()
+
+	def get_maintenance_log(self, task):
+		return frappe.get_value(
+			"Asset Maintenance Log_",
+			{
+				"asset_maintenance": self.name,
+				"task": task.name,
+				"maintenance_status": ("in",["Planned","Overdue"])
+			}
+		)
+
+	def create_new_maintenance_log(self, task):
+		asset_maintenance_log = frappe.get_doc({
+			"doctype": "Asset Maintenance Log_",
+			"asset_maintenance": self.name,
+			"asset_name": self.asset_name,
+			"task": task.name,
+			"has_certificate": task.certificate_required,
+			"description": task.description,
+			"assign_to_name": task.assign_to_name,
+			"periodicity": str(task.periodicity),
+			"maintenance_type": task.maintenance_type,
+			"due_date": task.next_due_date
+		})
+		asset_maintenance_log.insert()
+
+	def cancel_maintenance_logs_for_removed_tasks(self, tasks_names):
+		asset_maintenance_logs = self.get_maintenance_logs_for_removed_tasks(tasks_names)
+
+		if asset_maintenance_logs:
+			for asset_maintenance_log in asset_maintenance_logs:
+				frappe.db.set_value("Asset Maintenance Log_", asset_maintenance_log.name, "maintenance_status", "Cancelled")
+
+	def get_maintenance_logs_for_removed_tasks(self, tasks_names):
+		return frappe.get_all(
+			"Asset Maintenance Log_",
+			fields = ["name"],
+			filters = {
+				"asset_maintenance": self.name,
+				"task": ("not in", tasks_names)
+			}
+		)
+
