dc47e47d48e3318ae2f7afa5c40cfc3443bc7250
GangaManoj <ganga.manoj98@gmail.com>
2022-03-10 20:03:07 +0530

    fix: Validate asset maintenance log

--- assets/asset/doctype/asset_maintenance_log_/asset_maintenance_log_.py
+++ assets/asset/doctype/asset_maintenance_log_/asset_maintenance_log_.py
@@ -1,8 +1,26 @@
 # Copyright (c) 2021, Ganga Manoj and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils import getdate
 
 class AssetMaintenanceLog_(Document):
-	pass
+	def validate(self):
+		self.check_if_maintenance_is_overdue()
+		self.validate_completion_date()
+		self.validate_maintenance_status()
+
+	def check_if_maintenance_is_overdue(self):
+		if getdate(self.due_date) < getdate() and self.maintenance_status not in ["Completed", "Cancelled"]:
+			self.maintenance_status = "Overdue"
+
+	def validate_completion_date(self):
+		if self.maintenance_status == "Completed" and not self.completion_date:
+			frappe.throw(_("Please select Completion Date for Completed Asset Maintenance Log"))
+
+	def validate_maintenance_status(self):
+		if self.maintenance_status != "Completed" and self.completion_date:
+			frappe.throw(_("Please select Maintenance Status as Completed or remove Completion Date"))
+
