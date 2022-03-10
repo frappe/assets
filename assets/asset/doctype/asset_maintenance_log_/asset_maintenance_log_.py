8b31a5be53091a1653a3e713820c57304ee312b0
GangaManoj <ganga.manoj98@gmail.com>
2022-03-10 20:06:33 +0530

    fix: Validate maintenance status

--- assets/asset/doctype/asset_maintenance_log_/asset_maintenance_log_.py
+++ assets/asset/doctype/asset_maintenance_log_/asset_maintenance_log_.py
@@ -10,6 +10,8 @@ class AssetMaintenanceLog_(Document):
 	def validate(self):
 		self.check_if_maintenance_is_overdue()
 		self.validate_completion_date()
+
+	def on_submit(self):
 		self.validate_maintenance_status()
 
 	def check_if_maintenance_is_overdue(self):
@@ -20,7 +22,9 @@ class AssetMaintenanceLog_(Document):
 		if self.maintenance_status == "Completed" and not self.completion_date:
 			frappe.throw(_("Please select Completion Date for Completed Asset Maintenance Log"))
 
-	def validate_maintenance_status(self):
 		if self.maintenance_status != "Completed" and self.completion_date:
 			frappe.throw(_("Please select Maintenance Status as Completed or remove Completion Date"))
 
+	def validate_maintenance_status(self):
+		if self.maintenance_status not in ["Completed", "Cancelled"]:
+			frappe.throw(_("Maintenance Status has to be Cancelled or Completed to Submit this doc"))
