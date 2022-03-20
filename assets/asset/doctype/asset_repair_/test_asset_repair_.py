5e16876a2d712edd0c648c33accb518237d7da01
GangaManoj <ganga.manoj98@gmail.com>
2022-03-21 03:26:14 +0530

    test: Create setUpClass()

--- assets/asset/doctype/asset_repair_/test_asset_repair_.py
+++ assets/asset/doctype/asset_repair_/test_asset_repair_.py
@@ -1,8 +1,24 @@
 # Copyright (c) 2021, Ganga Manoj and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 import unittest
 
+from assets.asset.doctype.asset_.test_asset_ import (
+	create_asset,
+	create_company,
+	create_asset_data,
+)
+from erpnext.stock.doctype.item.test_item import create_item
+
 class TestAssetRepair_(unittest.TestCase):
-	pass
+	@classmethod
+	def setUpClass(cls):
+		create_company()
+		create_asset_data()
+		create_item("_Test Stock Item")
+		frappe.db.sql("delete from `tabTax Rule`")
+
+	@classmethod
+	def tearDownClass(cls):
+		frappe.db.rollback()
