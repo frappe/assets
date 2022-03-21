fe20e37678d6d812c7efcc8ff8f3aea098aaf511
GangaManoj <ganga.manoj98@gmail.com>
2022-03-22 04:29:50 +0530

    test: Test if Asset Movement is created on submitting an Asset

--- assets/asset/doctype/asset_movement_/test_asset_movement_.py
+++ assets/asset/doctype/asset_movement_/test_asset_movement_.py
@@ -1,8 +1,28 @@
 # Copyright (c) 2021, Ganga Manoj and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 import unittest
 
+from assets.asset.doctype.asset_.test_asset_ import (
+	create_asset,
+	create_company,
+	create_asset_data,
+)
+
 class TestAssetMovement_(unittest.TestCase):
-	pass
+	@classmethod
+	def setUpClass(cls):
+		create_company()
+		create_asset_data()
+
+	@classmethod
+	def tearDownClass(cls):
+		frappe.db.rollback()
+
+	def test_movement_is_created_on_asset_submission(self):
+		asset = create_asset(submit=1)
+		asset_movement = frappe.get_last_doc("Asset Movement_")
+
+		self.assertEqual(asset_movement.purpose, "Receipt")
+		self.assertEqual(asset_movement.assets[0].asset, asset.name)
